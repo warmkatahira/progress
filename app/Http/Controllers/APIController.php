ac751165ee64e71c4a557cb20bb3234be2573cec
warmkatahira <t.katahira@warm.co.jp>
2022-11-29 14:52:43 +0900

    second

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -15,11 +15,21 @@ class APIController extends Controller
 
     public function create(Request $request)
     {
-        $post = new Post;
-        $post->customer_code = $request->customer_code;
-        $post->info_1_label = $request->info_1_label;
-        $post->info_1_text = $request->info_1_text;
-        $post->save();
+        // postsテーブルにcustomer_codeがなければ追加、あれば更新
+        $post_count = Post::where('customer_code', $request->customer_code)->count();
+        if($post_count == 0){
+            $post = new Post;
+            $post->customer_code = $request->customer_code;
+            $post->info_1_label = $request->info_1_label;
+            $post->info_1_text = $request->info_1_text;
+            $post->save();
+        }
+        if($post_count != 0){
+            Post::where('customer_code', $request->customer_code)->update([
+                $post->info_1_label = $request->info_1_label;
+                $post->info_1_text = $request->info_1_text;
+            ]);
+        }
         return response()->json([
             "message" => "post record created"
         ], 201);
