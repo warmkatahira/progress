a025380665fd5a2bf4128a330a12da2f19b76858
warmkatahira <t.katahira@warm.co.jp>
2022-11-29 16:32:45 +0900

    third

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,6 +12,6 @@
             </ul>
         </div>
         <p class="col-start-3 col-span-8 text-2xl xl:text-5xl text-blue-500 text-center mt-2 xl:mt-0">進捗掲示板</p>
-        <p class="col-start-11 col-span-2 text-right text-xl mr-3 mt-3">{{ Auth::user()->name }}</p>
+        <p class="col-start-11 col-span-2 text-right text-sm xl:text-xl mr-3 mt-5 xl:mt-3">{{ Auth::user()->name }}</p>
     </div>
 </header>
