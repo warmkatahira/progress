9cc21cce5b8b98ef286b5e80d75ba4da9d2d9630
warmkatahira <t.katahira@warm.co.jp>
2023-04-25 15:29:34 +0900

    20230425

--- resources/views/post_list/detail.blade.php
+++ resources/views/post_list/detail.blade.php
@@ -16,12 +16,12 @@
                     <p id="progress" class="text-center large_disp border border-gray-600 py-2"></p>
                 </div>
                 <div class="flex flex-col xl:flex-row mt-0 xl:mt-2">
-                    <div class="flex flex-col">
+                    <div class="flex flex-col w-full">
                         <p class="text-3xl text-center small_disp bg-gray-600 py-2 text-white">{{ $post->info_1_label }}</p>
                         <p class="text-3xl text-center small_disp border border-gray-600 py-2">{{ number_format($post->info_1_text) }}</p>
                     </div>
                     @if(!is_null($post->info_2_label))
-                        <div class="flex flex-col ml-0 xl:ml-3">
+                        <div class="flex flex-col w-full ml-0 xl:ml-3">
                             <p class="text-3xl text-center small_disp bg-gray-600 py-2 text-white">{{ $post->info_2_label }}</p>
                             <p class="text-3xl text-center small_disp border border-gray-600 py-2">{{ number_format($post->info_2_text) }}</p>
                         </div>
@@ -29,13 +29,13 @@
                 </div>
                 <div class="flex flex-col xl:flex-row mt-0 xl:mt-2">
                     @if(!is_null($post->info_3_label))
-                        <div class="flex flex-col">
+                        <div class="flex flex-col w-full">
                             <p class="text-3xl text-center small_disp bg-gray-600 py-2 text-white">{{ $post->info_3_label }}</p>
                             <p class="text-3xl text-center small_disp border border-gray-600 py-2">{{ number_format($post->info_3_text) }}</p>
                         </div>
                     @endif
                     @if(!is_null($post->info_4_label))
-                        <div class="flex flex-col ml-0 xl:ml-3">
+                        <div class="flex flex-col w-full ml-0 xl:ml-3">
                             <p class="text-3xl text-center small_disp bg-gray-600 py-2 text-white">{{ $post->info_4_label }}</p>
                             <p class="text-3xl text-center small_disp border border-gray-600 py-2">{{ number_format($post->info_4_text) }}</p>
                         </div>
